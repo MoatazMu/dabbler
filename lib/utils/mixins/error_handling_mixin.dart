1c16f7b47c58e4c1093dbe2e458300399f5fc486
MoatazMu <moatazmustapha.ai@gmail.com>
2025-10-17 01:22:22 +0400

    chore(prune): remove unreferenced code files

--- lib/utils/mixins/error_handling_mixin.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-mixin ErrorHandlingMixin<T extends StatefulWidget> on State<T> {
-  void showError(String message) {
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(content: Text(message), backgroundColor: Colors.red),
-    );
-  }
-}
