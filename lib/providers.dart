259c8820401ec3d510114ed3708ebff28efb65d8
MoatazMu <moatazmustapha.ai@gmail.com>
2025-10-25 19:24:21 +0400

    10 bench_mode: add repository and providers (dart only)

--- lib/providers.dart
+++ lib/providers.dart
@@ -2,6 +2,7 @@ export 'features/venues/providers.dart';
 export 'features/sport_profiles/providers.dart';
 export 'services/supabase_service.dart';
 export 'features/profile/providers.dart';
+export 'features/bench_mode/providers.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:dabbler/services/supabase_service.dart';
